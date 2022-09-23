2ec3fea11df270e8bc4dc50485d1f6d33cca255b
Jesufemi Adetomiwa <olamideadetomiwa2@gmail.com>
2022-09-22 21:41:16 -0700

    0x06-pointers_arrays_strings

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,22 +1,23 @@
-#include "holberton.h"
+#include "main.h"
 /**
- * *_strncpy - copies a string
- * @dest: pointer to the copied string
- * @src: pointer to the string to be copied
- * @n: int
- * Return: destination
+ *_strncpy - copies a string
+ * @dest: destination to copy
+ * @src: source to copy
+ * @n: amount of places to copy
+ * Return: value to dest
 */
-
 char *_strncpy(char *dest, char *src, int n)
 {
 	int i;
 
 	for (i = 0; i < n && src[i] != '\0'; i++)
+	{
 		dest[i] = src[i];
-
+	}
 	for (; i < n; i++)
+	{
 		dest[i] = '\0';
-
+	}
 	return (dest);
 }
 
